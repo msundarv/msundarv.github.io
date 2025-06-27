e0eb6c7fe9d9c749dbb3b93686dde9ef0d8d2804
Sundar <msundarv@gmail.com>
2025-06-27 11:34:38 +0800

    Added Base Src

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# msundarv.github.io
+# My Personal Website
+
+This repository contains the source code for my personal website, hosted at [msundarv.com](https://msundarv.com).
+
+## 📫 Find Me Online
+- [Link](http://msundarv.com/) to my personal website 🏠
+- [LinkedIn](https://www.linkedin.com/in/msundarv) 💼 
+- [Medium](https://medium.com/@msundarv) ✍🏽
+- [Twitter](https://twitter.com/msundarv) 🐦
---
+++ js/projects.js
@@ -0,0 +1,200 @@
+ $(function(){
+      $('#btnDialog').click(function(){
+        $('#dialog').dialogBox({
+          hasClose: true,
+          effect: 'fade',
+          title: '<br>PikVu',
+          content: '<iframe width="420" height="345" src="https://www.youtube.com/embed/6pWJL6207I0"></iframe><br><br>"Google maps for your warehouse". <br>PikVu automates the entire "pick and pack" process in your warehouse using our Augmented Reality technology<br> and also provides a Real Time tracking system to identify exact location of each picker inside the warehouse.',
+          callback: function(){
+            console.log('loading over')
+          }
+        })
+      })
+    })
+
+    $(function(){
+      $('#btnDialog1').click(function(){
+        $('#dialog1').dialogBox({
+          hasClose: true,
+          effect: 'fade',
+          title: '<br>RemoteVu',
+          content: 'I am a dialog box! Please custom styles base on options!<br><br><iframe width="420" height="345" src="https://www.youtube.com/embed/XGSy3_Czz8k"></iframe><br><br>hi',
+          callback: function(){
+            console.log('loading over')
+          }
+        })
+      })
+    })
+
+     $(function(){
+      $('#btnDialog2').click(function(){
+        $('#dialog2').dialogBox({
+          hasClose: true,
+          effect: 'fade',
+          title: '<br>AR Headset',
+          content: 'I am a dialog box! Please custom styles base on options!<br><br><iframe width="420" height="345" src="https://www.youtube.com/embed/XGSy3_Czz8k"</iframe><br><br>',
+          callback: function(){
+            console.log('loading over')
+          }
+        })
+      })
+    })
+
+      $(function(){
+      $('#btnDialog3').click(function(){
+        $('#dialog3').dialogBox({
+          hasClose: true,
+          effect: 'fade',
+          title: '<br>AR CardGame',
+          content: 'I am a dialog box! Please custom styles base on options!<br><br><iframe width="420" height="345" src="https://www.youtube.com/embed/XGSy3_Czz8k"</iframe><br><br>',
+          callback: function(){
+            console.log('loading over')
+          }
+        })
+      })
+    })
+
+
+
+      $(function(){
+      $('#btnDialog10').click(function(){
+        $('#dialog10').dialogBox({
+          hasClose: true,
+          effect: 'fade',
+          title: '<br>Air Hockey',
+          content: 'I am a dialog box! Please custom styles base on options!<br><br><iframe width="420" height="345" src="https://www.youtube.com/embed/XGSy3_Czz8k"</iframe><br><br>',
+          callback: function(){
+            console.log('loading over')
+          }
+        })
+      })
+    })
+
+       $(function(){
+      $('#btnDialog11').click(function(){
+        $('#dialog11').dialogBox({
+          hasClose: true,
+          effect: 'fade',
+          title: '<br>Social VR',
+          content: 'I am a dialog box! Please custom styles base on options!<br><br><iframe width="420" height="345" src="https://www.youtube.com/embed/XGSy3_Czz8k"</iframe><br><br>',
+          callback: function(){
+            console.log('loading over')
+          }
+        })
+      })
+    })
+
+        $(function(){
+      $('#btnDialog12').click(function(){
+        $('#dialog12').dialogBox({
+          hasClose: true,
+          effect: 'fade',
+          title: '<br>Telepresence',
+          content: 'I am a dialog box! Please custom styles base on options!<br><br><iframe width="420" height="345" src="https://www.youtube.com/embed/XGSy3_Czz8k"</iframe><br><br>',
+          callback: function(){
+            console.log('loading over')
+          }
+        })
+      })
+    })
+
+         $(function(){
+      $('#btnDialog13').click(function(){
+        $('#dialog13').dialogBox({
+          hasClose: true,
+          effect: 'fade',
+          title: '<br>EasyG',
+          content: 'I am a dialog box! Please custom styles base on options!<br><br><iframe width="420" height="345" src="https://www.youtube.com/embed/XGSy3_Czz8k"</iframe><br><br>',
+          callback: function(){
+            console.log('loading over')
+          }
+        })
+      })
+    })
+
+          $(function(){
+      $('#btnDialog14').click(function(){
+        $('#dialog14').dialogBox({
+          hasClose: true,
+          effect: 'fade',
+          title: '<br>Furniture AR',
+          content: 'I am a dialog box! Please custom styles base on options!<br><br><iframe width="420" height="345" src="https://www.youtube.com/embed/XGSy3_Czz8k"</iframe><br><br>',
+          callback: function(){
+            console.log('loading over')
+          }
+        })
+      })
+    })
+
+
+
+          $(function(){
+      $('#btnDialog15').click(function(){
+        $('#dialog15').dialogBox({
+          hasClose: true,
+          effect: 'fade',
+          title: '<br>AR Training',
+          content: 'I am a dialog box! Please custom styles base on options!<br><br><iframe width="420" height="345" src="https://www.youtube.com/embed/XGSy3_Czz8k"</iframe><br><br>',
+          callback: function(){
+            console.log('loading over')
+          }
+        })
+      })
+    })
+
+
+           $(function(){
+      $('#btnDialog20').click(function(){
+        $('#dialog20').dialogBox({
+          hasClose: true,
+          effect: 'fade',
+          title: '<br>ASE Apps',
+          content: 'I am a dialog box! Please custom styles base on options!<br><br><iframe width="420" height="345" src="https://www.youtube.com/embed/XGSy3_Czz8k"</iframe><br><br>',
+          callback: function(){
+            console.log('loading over')
+          }
+        })
+      })
+    })
+
+       $(function(){
+      $('#btnDialog21').click(function(){
+        $('#dialog21').dialogBox({
+          hasClose: true,
+          effect: 'fade',
+          title: '<br>Anokha AR',
+          content: 'I am a dialog box! Please custom styles base on options!<br><br><iframe width="420" height="345" src="https://www.youtube.com/embed/XGSy3_Czz8k"</iframe><br><br>',
+          callback: function(){
+            console.log('loading over')
+          }
+        })
+      })
+    })
+
+        $(function(){
+      $('#btnDialog22').click(function(){
+        $('#dialog22').dialogBox({
+          hasClose: true,
+          effect: 'fade',
+          title: '<br>TN HelpDesk',
+          content: 'I am a dialog box! Please custom styles base on options!<br><br><iframe width="420" height="345" src="https://www.youtube.com/embed/XGSy3_Czz8k"</iframe><br><br>',
+          callback: function(){
+            console.log('loading over')
+          }
+        })
+      })
+    })
+
+         $(function(){
+      $('#btnDialog23').click(function(){
+        $('#dialog23').dialogBox({
+          hasClose: true,
+          effect: 'fade',
+          title: '<br>LWP',
+          content: 'I am a dialog box! Please custom styles base on options!<br><br><iframe width="420" height="345" src="https://www.youtube.com/embed/XGSy3_Czz8k"</iframe><br><br>',
+          callback: function(){
+            console.log('loading over')
+          }
+        })
+      })
+    })
